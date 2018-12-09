2c462d6a0169670186b3879e903ea456cfd650fe
1hausarzt1 <45625409+1hausarzt1@users.noreply.github.com>
2018-12-09 12:01:24 +0100

    Update map.js

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@
         // Styles a map in night mode.
         var map = new google.maps.Map(document.getElementById('map'), {
           center:  {lat: 53.5573, lng: 9.97165},
-          zoom: 17,
+          zoom: 18,
           
 		  
 		 styles: [{"featureType":"all","elementType":"all","stylers":[{"saturation":-100},{"gamma":0.7}]},
